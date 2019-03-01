711ec0a7a3b4b900b67612350c2e04f36034b169
Ivan Krizsan <ivan@ivankrizsan.se>
2019-03-01 21:24:56 +0100

    Added subscribable message channel examples.

---
+++ src/test/java/se/ivankrizsan/springintegration/messagechannels/subscribable/SubscribableChannelsTests.java
@@ -0,0 +1,286 @@
+package se.ivankrizsan.springintegration.messagechannels.subscribable;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.integration.channel.AbstractSubscribableChannel;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageDeliveryException;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.SubscribableChannel;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import se.ivankrizsan.springintegration.shared.EmptyConfiguration;
+import se.ivankrizsan.springintegration.shared.SpringIntegrationExamplesConstants;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.TimeUnit;
+
+import static org.awaitility.Awaitility.await;
+
+/**
+ * Exercises demonstrating common properties of Spring Integration subscribable message channels.
+ *
+ * @author Ivan Krizsan
+ */
+@SpringBootTest
+@EnableIntegration
+@SpringJUnitConfig(classes = { EmptyConfiguration.class })
+public class SubscribableChannelsTests implements SpringIntegrationExamplesConstants {
+    /* Class variable(s): */
+    protected static final Log LOGGER = LogFactory.getLog(SubscribableChannelsTests.class);
+
+    /* Instance variable(s): */
+
+    /**
+     * Tests creating a subscribable message channel and sending a message to it
+     * without any subscribers being subscribed.
+     * Expected result:
+     * An exception should be thrown indicating that the message could not be delivered.
+     */
+    @Test
+    public void noSubscribersTest() {
+        final SubscribableChannel theSubscribableChannel;
+        final Message<String> theInputMessage;
+
+        theInputMessage = MessageBuilder
+            .withPayload(GREETING_STRING)
+            .build();
+
+        // <editor-fold desc="Answer Section" defaultstate="collapsed">
+        theSubscribableChannel = new DirectChannel();
+        /*
+         * Give the message channel a name so that it will
+         * appear in any related log messages.
+         */
+        ((AbstractSubscribableChannel)theSubscribableChannel)
+            .setBeanName("MessageChannelWithNoSubscribers");
+
+        Assertions.assertThrows(MessageDeliveryException.class, () ->
+            theSubscribableChannel.send(theInputMessage));
+        // </editor-fold>
+    }
+
+    /**
+     * Tests creating a subscribable message channel and subscribing one
+     * subscriber to the channel. A message is then sent to the channel.
+     * Expected result:
+     * The single subscriber should receive the message sent to the subscribable
+     * message channel.
+     */
+    @Test
+    public void singleSubscriberTest() {
+        final SubscribableChannel theSubscribableChannel;
+        final Message<String> theInputMessage;
+        final List<Message> theSubscriberReceivedMessages =
+            new CopyOnWriteArrayList<>();
+
+        theInputMessage = MessageBuilder
+            .withPayload(GREETING_STRING)
+            .build();
+
+        // <editor-fold desc="Answer Section" defaultstate="collapsed">
+        theSubscribableChannel = new DirectChannel();
+        /*
+         * Give the message channel a name so that it will
+         * appear in any related log messages.
+         */
+        ((AbstractSubscribableChannel)theSubscribableChannel)
+            .setBeanName("MessageChannelWithSingleSubscriber");
+
+        /*
+         * Create a subscriber (message handler) that adds each received
+         * message to a list.
+         */
+        final MessageHandler theSubscriber = theSubscriberReceivedMessages::add;
+
+        /* Register the subscriber with the subscribable message channel. */
+        final boolean theSubscribedFlag = theSubscribableChannel.subscribe(theSubscriber);
+
+        Assertions.assertTrue(theSubscribedFlag);
+
+        theSubscribableChannel.send(theInputMessage);
+        // </editor-fold>
+        await()
+            .atMost(2, TimeUnit.SECONDS)
+            .until(() ->
+                theSubscriberReceivedMessages.size() > 0);
+
+        /*
+         * The subscriber that subscribed to the subscribable message channel
+         * prior to the message was sent to the message channel should receive the
+         * message.
+         */
+        LOGGER.info("Subscriber received message: " + theSubscriberReceivedMessages.get(0));
+        Assertions.assertEquals(
+            1,
+            theSubscriberReceivedMessages.size(),
+            "A single message should have been received by the subscriber");
+    }
+
+    /**
+     * Tests creating a subscribable message channel and subscribing two
+     * subscribers to the channel. A message is then sent to the channel.
+     * Expected result:
+     * One single message should have been received by the first message
+     * channel subscriber. No message should have been received by the
+     * second subscriber.
+     */
+    @Test
+    public void multipleSubscribersTest() {
+        final SubscribableChannel theSubscribableChannel;
+        final Message<String> theInputMessage;
+        final List<Message> theFirstSubscriberReceivedMessages =
+            new CopyOnWriteArrayList<>();
+        final List<Message> theSecondSubscriberReceivedMessages =
+            new CopyOnWriteArrayList<>();
+
+        theInputMessage = MessageBuilder
+            .withPayload(GREETING_STRING)
+            .build();
+
+        // <editor-fold desc="Answer Section" defaultstate="collapsed">
+        theSubscribableChannel = new DirectChannel();
+        /*
+         * Give the message channel a name so that it will
+         * appear in any related log messages.
+         */
+        ((AbstractSubscribableChannel)theSubscribableChannel)
+            .setBeanName("MessageChannelWithMultipleSubscribers");
+
+        /*
+         * Create two subscribers (message handler) that adds each received
+         * message to a list.
+         */
+        final MessageHandler theFirstSubscriber =
+            theFirstSubscriberReceivedMessages::add;
+        final MessageHandler theSecondSubscriber =
+            theSecondSubscriberReceivedMessages::add;
+
+        /* Register the subscribers with the subscribable message channel. */
+        final boolean theFirstSubscribedFlag = theSubscribableChannel.subscribe(theFirstSubscriber);
+        final boolean theSecondSubscribedFlag = theSubscribableChannel.subscribe(theSecondSubscriber);
+
+        Assertions.assertTrue(theFirstSubscribedFlag);
+        Assertions.assertTrue(theSecondSubscribedFlag);
+
+        theSubscribableChannel.send(theInputMessage);
+        // </editor-fold>
+        await()
+            .atMost(2, TimeUnit.SECONDS)
+            .until(() ->
+                theFirstSubscriberReceivedMessages.size() > 0);
+
+        /*
+         * Since the message channel is a subscribable channel, only one subscriber
+         * will receive each message sent to the channel.
+         * In this test, since only one message is sent to the channel, only one
+         * of the subscribers will receive a message.
+         */
+        Assertions.assertEquals(
+            1,
+            theFirstSubscriberReceivedMessages.size(),
+            "A single message should have been received by first subscriber");
+        Assertions.assertEquals(
+            0,
+            theSecondSubscriberReceivedMessages.size(),
+            "No message should have been received by second subscriber");
+    }
+
+    /**
+     * Tests creating a subscribable message channel and subscribing two
+     * subscribers to the channel. A message is then sent to the channel.
+     * Then the first subscriber is unsubscribed from the message channel and
+     * another message is sent to the channel.
+     * Expected result:
+     * The first message should be received by the first subscriber
+     * and the second message should be received by the second subscriber.
+     */
+    @Test
+    public void unsubscribeTest() {
+        final SubscribableChannel theSubscribableChannel;
+        final Message<String> theFirstInputMessage;
+        final Message<String> theSecondInputMessage;
+        final List<Message> theFirstSubscriberReceivedMessages =
+            new CopyOnWriteArrayList<>();
+        final List<Message> theSecondSubscriberReceivedMessages =
+            new CopyOnWriteArrayList<>();
+
+        theFirstInputMessage = MessageBuilder
+            .withPayload(GREETING_STRING + "1")
+            .build();
+        theSecondInputMessage = MessageBuilder
+            .withPayload(GREETING_STRING + "2")
+            .build();
+
+        // <editor-fold desc="Answer Section" defaultstate="collapsed">
+        theSubscribableChannel = new DirectChannel();
+        /*
+         * Give the message channel a name so that it will
+         * appear in any related log messages.
+         */
+        ((AbstractSubscribableChannel)theSubscribableChannel)
+            .setBeanName("MessageChannelToUnsubscribeFrom");
+
+        /*
+         * Create two subscribers (message handler) that adds each received
+         * message to a list.
+         */
+        final MessageHandler theFirstSubscriber = theFirstSubscriberReceivedMessages::add;
+        final MessageHandler theSecondSubscriber = theSecondSubscriberReceivedMessages::add;
+
+        /* Register the subscribers with the subscribable message channel. */
+        theSubscribableChannel.subscribe(theFirstSubscriber);
+        theSubscribableChannel.subscribe(theSecondSubscriber);
+
+        LOGGER.info("Number of subscribers before unsubscribe: "
+            + ((AbstractSubscribableChannel)theSubscribableChannel).getSubscriberCount());
+
+        /* Send the first message to the subscribable message channel. */
+        theSubscribableChannel.send(theFirstInputMessage);
+
+        /* Unsubscribe the first subscriber from the message channel. */
+        theSubscribableChannel.unsubscribe(theFirstSubscriber);
+
+        LOGGER.info("Number of subscribers after unsubscribe: "
+            + ((AbstractSubscribableChannel)theSubscribableChannel).getSubscriberCount());
+
+        /* Send the second message to the subscribable message channel. */
+        theSubscribableChannel.send(theSecondInputMessage);
+
+        // </editor-fold>
+        await()
+            .atMost(2, TimeUnit.SECONDS)
+            .until(() ->
+                !theFirstSubscriberReceivedMessages.isEmpty() && !theSecondSubscriberReceivedMessages.isEmpty());
+
+        /* Each subscriber should have received one message. */
+        Assertions.assertEquals(
+            1,
+            theFirstSubscriberReceivedMessages.size(),
+            "First subscriber should have received one message");
+        Assertions.assertEquals(
+            1,
+            theSecondSubscriberReceivedMessages.size(),
+            "Second subscriber should have received one message");
+
+        /* Check message payloads. */
+        Assertions.assertEquals(
+            GREETING_STRING + "1",
+            theFirstSubscriberReceivedMessages
+                .get(0)
+                .getPayload(),
+            "First subscriber should have received the first message");
+        Assertions.assertEquals(
+            GREETING_STRING + "2",
+            theSecondSubscriberReceivedMessages
+                .get(0)
+                .getPayload(),
+            "Second subscriber should have received the second message");
+    }
+}
